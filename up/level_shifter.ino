745aa2f80757914ecb67008b1d658a9b399e1232
ncss136 <ncss136@3f877a8c-bed2-0410-bb75-8f1df17e47a6>
2013-01-11 05:37:27 +0000

    first version of the level shifting software. feel free to try and make it work better!
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss136@4 3f877a8c-bed2-0410-bb75-8f1df17e47a6

---
+++ up/level_shifter.ino
@@ -0,0 +1,35 @@
+#define ANA_PIN A0
+#define REF_PIN A1
+#define MESSAGE_PIN 7
+
+int ref_signal = 0;
+int signal = 0;
+byte demod_signal = LOW;
+
+void setup()
+{
+  pinMode(MESSAGE_PIN, OUTPUT);
+  
+  Serial.begin(9600);
+  
+}
+
+void loop()
+{
+  ref_signal = analogRead(REF_PIN);
+  signal = analogRead(ANA_PIN);
+  
+  demod_signal = signal > ref_signal ? HIGH : LOW;
+  /*
+  Serial.print("Mod: ");
+  Serial.print(signal, DEC);
+  
+  Serial.print(", Ref: ");
+  Serial.println(ref_signal,DEC);
+  
+  Serial.print("\tDemod: ");
+  Serial.println(demod_signal,DEC);
+  */
+  digitalWrite(MESSAGE_PIN, demod_signal);
+}
+
