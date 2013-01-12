6b88c28543d421e55e9b1098147e352af78bdc4e
ncss136 <ncss136@3f877a8c-bed2-0410-bb75-8f1df17e47a6>
2013-01-12 04:36:02 +0000

    Recommit
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss136@44 3f877a8c-bed2-0410-bb75-8f1df17e47a6

---
+++ derp/bittrans/bitstream.cpp
@@ -0,0 +1,52 @@
+#include "bitstream.h"
+
+void bitStream::fromBitStream(char *stream)
+{
+  message msg = *(message *)stream;
+  latitude.deg = msg.latdeg;
+  if (msg.latsign) latitude.deg *= -1;
+  latitude.min = msg.latmin;
+  latitude.sec = msg.latsec;
+  
+  longitude.deg = msg.longdeg;
+  if (msg.longsign) longitude.deg *= -1;
+  longitude.min = msg.longmin;
+  longitude.sec = msg.longsec;
+  
+  altitude = msg.alt;
+  temperature = msg.temp;
+  chksum = msg.chksum;
+}
+
+void bitStream::toBitStream(ordinate _lat, ordinate _long, int _alt, int _temp, char _chksum)
+{
+  message msg;
+  if (_lat.deg < 0)
+  {
+    msg.latsign = 1;
+    _lat.deg *= -1;
+  }
+  else msg.latsign = 0;
+  msg.latdeg = _lat.deg;
+  msg.latmin = _lat.min;
+  msg.latsec = _lat.sec;
+  
+  if (_long.deg < 0)
+  {
+    msg.longsign = 1;
+    _long.deg *= -1;
+  }
+  else msg.longsign = 0;
+  msg.longdeg = _long.deg;
+  msg.longmin = _long.min;
+  msg.longsec = _long.sec;
+  
+  msg.alt = _alt;
+  msg.temp = _temp;
+  msg.chksum = _chksum;
+  char *charstream = (char*)&msg;
+  for (int i = 0; i < NUMBYTES; i++)
+  {
+    stream[i] = charstream[i];
+  }
+}
--- derp/bittrans/bittrans.ino
+++ derp/bittrans/bittrans.ino
@@ -3,35 +3,72 @@
 #include "bitstream.h"
 #define SENSOR_PIN 2
 #define REFV 700
+#define DELAY (1000/10)
 
 int prev = -1;
 int now = 0;
+
+void ordPrint(ordinate inord){
+  Serial.print("Degrees: ");
+  Serial.println(inord.deg);
+  Serial.print("Minutes: ");
+  Serial.println(inord.min);
+  Serial.print("Seconds: ");
+  Serial.println(inord.sec);
+}
+
+void printBitStream(bitStream ibs){
+  Serial.println("\nLatitude:\n");
+  ordPrint(ibs.getLatitude());
+  Serial.println("\nLongitude:\n");
+  ordPrint(ibs.getLongitude());
+  Serial.print("Altitude: ");
+  Serial.println(ibs.getAltitude());
+  Serial.print("Temperature:");
+  Serial.println(ibs.getTemperature());
+}
+
+void printBin(char *iarr, int len){
+	for (int i = 0; i < len; i++){
+    	for (int j = 0; j < 8; j++)
+    	{
+    		char cbyte = iarr[i];
+    		char mask = (1<<j);
+    		bool bit = !!(cbyte&mask);
+    		Serial.print(bit);
+    	}
+    	Serial.print(" ");
+    }
+    Serial.println("");
+}
 
 void setup(){
 	Serial.begin(9600);
 }
 
 void loop(){
-	unsigned char ibstream[8];
+	char ibstream[8] = {0};
 	bitStream obstream;
 	// looks for a low to high transition in the signal
-	prev = (analogRead(SENSOR_PIN)>REFV)?1:0;
-	now = (analogRead(SENSOR_PIN)>REFV)?1:0;
-	if (prev == 0 && now == 1){
-		// for (int i = 0; i < 3; i++){
-		for (int j = 0; j < 8; j++){
-			for (int k = 0; k < 8; k++){
-				int blv = (analogRead(SENSOR_PIN) > REFV)?1:0;
-				ibstream[i][j] |= (blv << k);
-				delaymicroseconds(333);
-			}
+	while (analogRead(SENSOR_PIN) < REFV);
+	delay((DELAY*3)/2);
+	long stime = millis();
+	// for (int i = 0; i < 3; i++){
+	for (int j = 0; j < 8; j++){
+		for (int k = 0; k < 8; k++){
+			int blv = (analogRead(SENSOR_PIN) > REFV)?1:0;
+			ibstream[j] |= (blv << k);
+			while (millis()-stime < DELAY);
+			stime = millis();
 		}
-		// }
-		obstream.fromBitStream(ibstream);
-		boolean strue = (obstream.getChecksum() == checkSum(ibstream));
-		if (strue)
-			printBitStream(obstream);
 	}
+	// }
+	printBin(ibstream, 8);
+	obstream.fromBitStream(ibstream);
+	// boolean strue = (checkSum(ibstream) == 0);
+	// if (strue)
+	printBitStream(obstream);
+
 }
 
 /*unsigned char xorCheck(unsigned char *arr, int n)
