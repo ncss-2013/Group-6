cc2c5e4651f220dfed9f825627321ccb1b19d561
ncss136 <ncss136@3f877a8c-bed2-0410-bb75-8f1df17e47a6>
2013-01-12 11:31:44 +0000

    safety for serial
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss136@70 3f877a8c-bed2-0410-bb75-8f1df17e47a6

--- up/pc/pyRecieve/receiver.py
+++ up/pc/pyRecieve/receiver.py
@@ -1,38 +1,41 @@
 import serial
 import earth
 import time
+import string
 
 ORIGIN_LAT = -121998
 ORIGIN_LONG = 544299
 FILENAME = "log.txt"
 
 ser = serial.Serial(input("Enter COM port: "), 9600)
+try:
+    graph = earth.Graph()
+    while 1:
+        data = [0 for i in range(3)]
+        commas = 0
+        cur = 0
+        while commas < 3:
+            while not ser.available:
+                pass
+            char = ser.read()
+            if char == ',':
+                data[commas] = cur
+                commas += 1
+            elif char in string.digits:
+                cur *= 10
+                cur += int(chr(ser.read()))
+        f = open(FILENAME, 'a').write(','.join(data)+',\n')
+        del f
+        
+        point = earth.Point()
+        point.longitude = data[0]
+        point.latitude = data[1]
+        point.altitude = data[2]
+        #point.temperature = data[3]
+        point.timestamp = time.time()
 
-graph = earth.Graph()
-while 1:
-    data = [0 for i in range(3)]
-    commas = 0
-    cur = 0
-    while commas < 3:
-        while !ser.available:
-            pass
-        char = ser.read()
-        if char == ',':
-            data[commas] = cur
-            commas += 1
-        else:
-            cur *= 10
-            cur += int(chr(ser.read()))
-    f = open(FILENAME, 'a').write(','.join(data)+',\n')
-    del f
-    
-    point = earth.Point()
-    point.longitude = data[0]
-    point.latitude = data[1]
-    point.altitude = data[2]
-    #point.temperature = data[3]
-    point.timestamp = time.time()
-
-    graph.add_point(point)
-    
-    graph.save()
+        graph.add_point(point)
+        
+        graph.save()
+finally:
+    del ser
