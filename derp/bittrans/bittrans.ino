d22c933b1a1c2d962edf29d433b1b62543835662
ncss136 <ncss136@3f877a8c-bed2-0410-bb75-8f1df17e47a6>
2013-01-12 01:12:11 +0000

    Bugfix! NOT TESTED
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss136@33 3f877a8c-bed2-0410-bb75-8f1df17e47a6

--- derp/bittrans/bittrans.ino
+++ derp/bittrans/bittrans.ino
@@ -1,48 +1,43 @@
 // Search for a low to high transition and read the bit into 3 10-long byte arrays
 
+#include "bitstream.h"
 #define SENSOR_PIN 2
 #define REFV 700
 
 int prev = -1;
 int now = 0;
-unsigned char ibstream[3][10];
 
 void setup(){
 	Serial.begin(9600);
 }
 
 void loop(){
-	static bitStream obstream;
-	static bitStream obstream2;
-	static bitStream obstream3;
+	unsigned char ibstream[8];
+	bitStream obstream;
+	// looks for a low to high transition in the signal
 	prev = (analogRead(SENSOR_PIN)>REFV)?1:0;
 	now = (analogRead(SENSOR_PIN)>REFV)?1:0;
 	if (prev == 0 && now == 1){
-		for (int i = 0; i < 3; i++){
-			for (int j = 0; j < 10; j++){
-				for (int k = 0; k < 8; k++){
-					int blv = (analogRead(SENSOR_PIN) > REFV)?1:0;
-					ibstream[i][j] |= (blv << k);
-					delaymicroseconds(333);
-				}
+		// for (int i = 0; i < 3; i++){
+		for (int j = 0; j < 8; j++){
+			for (int k = 0; k < 8; k++){
+				int blv = (analogRead(SENSOR_PIN) > REFV)?1:0;
+				ibstream[i][j] |= (blv << k);
+				delaymicroseconds(333);
 			}
 		}
-		boolean strue[3];
-		for (int i = 0; i < 3; i++)
-			strue[i] = (xorCheck == ibstream[i][9]); 
-		if (strue[0])
-			obstream.fromBitStream(ibstream[0]);
-		if (strue[1])
-			obstream2.fromBitStream(ibstream[1]);
-		if (strue[2])
-			obstream3.fromBitStream(ibstream[2]);
+		// }
+		obstream.fromBitStream(ibstream);
+		boolean strue = (obstream.getChecksum() == checkSum(ibstream));
+		if (strue)
+			printBitStream(obstream);
 	}
 }
 
-unsigned char xorCheck(unsigned char *arr, int n)
+/*unsigned char xorCheck(unsigned char *arr, int n)
 {
 	unsigned char chk = arr[0];
 	for (int i= 1; i < n; i++)
 		chk ^= arr[i];
 	return chk;
-}
+}*/
