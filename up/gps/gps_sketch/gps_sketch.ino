394420c34863cc19e5b79ae66133776e2dabf471
ncss136 <ncss136@3f877a8c-bed2-0410-bb75-8f1df17e47a6>
2013-01-12 04:02:10 +0000

    finished GPS sending to transmitter, need to test.
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss136@39 3f877a8c-bed2-0410-bb75-8f1df17e47a6

---
+++ up/gps/gps_sketch/gps_sketch.ino
@@ -0,0 +1,48 @@
+#include <SoftwareSerial.h>
+#include <GlobalPositioning.h>
+#include <encoding.h>
+
+#define WAIT_TIME (12*60*1000)
+#define WARNING_TIME (5*1000)
+
+#define WARN_BYTE 0x55
+
+#define WRITE_BUFFER_SIZE 10
+#define WORD_SIZE 6
+
+GlobalPositioning gps(8, 9, 4800);
+
+long originLat, originLong;
+
+char writeBuffer[WRITE_BUFFER_SIZE];
+
+void setup() {
+  Serial.begin(9600);
+  
+  while (gps.readData() != 0)
+    ;
+  originLat = coordToSeconds(gps.latDeg, gps.latMin,
+                             gps.latSec);
+  originLong = coordToSeconds(gps.longDeg, gps.longMin,
+                              gps.longSec);
+}
+
+void loop() {
+  unsigned long waitUntil = millis() + WARNING_TIME;
+  unsigned long sendAt = millis() + WAIT_TIME;
+  while (waitUntil < millis()) 
+    gps.readData();
+  Serial.write(WARN_BYTE);
+  
+  long latitude, longitude, altitude;
+  latitude = coordToSeconds(gps.latDeg, gps.latMin,
+                            gps.latSec);
+  longitude = coordToSeconds(gps.longDeg, gps.longMin,
+                             gps.longSec);
+  altitude = altitude;
+  packWord(writeBuffer, latitude - originLat, longitude - originLong, altitude);
+  addChecksum(writeBuffer);
+  while (sendAt < millis())
+    ;
+  Serial.write(writeBuffer, WORD_SIZE);
+}
