75dbdc728805bc100ae6dedd08dc59dcedec3c2d
ncss136 <ncss136@3f877a8c-bed2-0410-bb75-8f1df17e47a6>
2013-01-12 11:27:02 +0000

    made receiver.py code work.
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss136@68 3f877a8c-bed2-0410-bb75-8f1df17e47a6

--- up/pc/pyRecieve/receiver.py
+++ up/pc/pyRecieve/receiver.py
@@ -1,13 +1,14 @@
 import serial
+import earth
+import time
 
 ORIGIN_LAT = -121998
 ORIGIN_LONG = 544299
-COM = 'COM9'
 FILENAME = "log.txt"
 
-ser = serial.Serial(COM, 9600)
+ser = serial.Serial(input("Enter COM port: "), 9600)
 
-
+graph = earth.Graph()
 while 1:
     data = [0 for i in range(3)]
     commas = 0
@@ -24,6 +25,14 @@ while 1:
             cur += int(chr(ser.read()))
     f = open(FILENAME, 'a').write(','.join(data)+',\n')
     del f
-    # TODO: Write to kml
-
     
+    point = earth.Point()
+    point.longitude = data[0]
+    point.latitude = data[1]
+    point.altitude = data[2]
+    #point.temperature = data[3]
+    point.timestamp = time.time()
+
+    graph.add_point(point)
+    
+    graph.save()
