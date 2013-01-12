43815c5fb74c823fa73722f925c397c6a77b13a0
ncss136 <ncss136@3f877a8c-bed2-0410-bb75-8f1df17e47a6>
2013-01-12 04:15:21 +0000

    EEPROM read/write functions implemented
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss136@42 3f877a8c-bed2-0410-bb75-8f1df17e47a6

---
+++ up/gps/eeprom.cpp
@@ -0,0 +1,44 @@
+
+#include <EEPROM.h>
+
+void writeEEPROM(long latitude, long longitude)
+{
+	byte temp[4] = {0,0,0,0};
+
+	// turn latitude into an array of bytes
+	temp[0] = latitude >> 24;
+	temp[1] = (latitude >> 16) & 15;
+	temp[2] = (latitude >> 8) & 15;
+	temp[3] = latitude & 15;
+
+	// write latitude to EEPROM
+	EEPROM.write(0, temp[0]);
+	EEPROM.write(1, temp[1]);
+	EEPROM.write(2, temp[2]);
+	EEPROM.write(3, temp[3]);
+
+	// turn longitude into an array of bytes
+	temp[0] = longitude >> 24;
+	temp[1] = (longitude >> 16) & 15;
+	temp[2] = (longitude >> 8) & 15;
+	temp[3] = longitude & 15;
+
+	// write longitude to EEPROM
+	EEPROM.write(4, temp[0]);
+	EEPROM.write(5, temp[1]);
+	EEPROM.write(6, temp[2]);
+	EEPROM.write(7, temp[3]);
+}
+
+void readEEPROM(long* latitude, long* longitude)
+{
+	*latitude = (EEPROM.read(0) * 4096) +
+		(EEPROM.read(1) * 256) +
+		(EEPROM.read(2) * 16) +
+		EEPROM.read(3);
+
+	*longitude = (EEPROM.read(4) * 4096) +
+		(EEPROM.read(5) * 256) +
+		(EEPROM.read(6) * 16) +
+		EEPROM.read(7);
+}
